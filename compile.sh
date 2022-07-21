d2999775d7b3aea6e2253f346f5ed7ad1fe69561
OmerDesezar <56995820+OmerDesezar@users.noreply.github.com>
2022-07-21 10:37:45 +0300

    Update compile.sh

--- compile.sh
+++ compile.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-gcc -ansi -I include -pedantic-errors -Wall -Wextra -g source/knights_tour.c test/knights_tour_test.c -fPIC -lbitarray -L. -Wl,-rpath="\$ORIGIN"
+gcc -ansi -I include -pedantic-errors -Wall -Wextra -g source/knights_tour.c test/knights_tour_test.c -fPIC -lbitarray -L. -Wl,-rpath="\$ORIGIN" -o knights_tour.out
 
